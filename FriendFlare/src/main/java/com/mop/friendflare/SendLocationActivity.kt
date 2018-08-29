5ddb26206dc7b7aee3a20db77846f7eed797892d
Matthew Paulin <paulin@Matthews-MacBook-Pro.local>
2018-08-29 14:40:28 -0700

    I can send sage a message

--- FriendFlare/src/main/java/com/mop/friendflare/SendLocationActivity.kt
+++ FriendFlare/src/main/java/com/mop/friendflare/SendLocationActivity.kt
@@ -97,6 +97,30 @@ class SendLocationActivity : AppCompatActivity(), View.OnClickListener, com.goog
         } catch (ex: Exception) {
         }
 
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                            Manifest.permission.SEND_SMS)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+ 
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        arrayOf(Manifest.permission.SEND_SMS),
+                        42)
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+        }
+
         btGetGeo.setOnClickListener(this)
 
         btFormURL.setOnClickListener {
